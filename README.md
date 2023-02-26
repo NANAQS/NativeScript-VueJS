c518e7258a750f833d1e17e2aa3010e47886dca0
NANAQS <102193397+NANAQS@users.noreply.github.com>
2023-02-25 23:02:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NativeScript-VueJS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-hleq3o)
---
+++ app/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <Page>
+    <ActionBar title="NativeFlix" />
+    <GridLayout>
+      <Label
+        class="text-xl align-middle text-center text-gray-500"
+        :text="message"
+        @tap="logMessage"
+      />
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+
+export default Vue.extend({
+  computed: {
+    message() {
+      return 'Blank Nanak app';
+    },
+  },
+
+  methods: {
+    logMessage() {
+      console.log('You have tapped the message!');
+    },
+  },
+});
+</script>
